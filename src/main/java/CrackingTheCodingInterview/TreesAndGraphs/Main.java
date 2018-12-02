c764c4314eae4aa6b0c5d8bb804682c98086c2dd
mael <mnana@takima.fr>
2018-12-02 15:57:44 +0100

    wip: changing .gitignore and working on trees and graphs

--- src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Main.java
+++ src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Main.java
@@ -1,4 +1,44 @@
 package CrackingTheCodingInterview.TreesAndGraphs;
 
 public class Main {
+    public static void main(String[] args) {
+        Node root = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+
+        root.adjacent = new Node[]{
+                node1,
+                node2
+        };
+
+        node1.adjacent = new Node[]{
+                node2,
+                node3
+        };
+
+        node2.adjacent = new Node[]{
+                node4
+        };
+
+        node3.adjacent = new Node[]{
+                node2,
+                node6
+        };
+
+        node4.adjacent = new Node[]{
+                node3,
+                node6,
+                node5,
+        };
+
+//        Node.searchBFS(root);
+//        Node.searchDFS(root);
+
+        System.out.println(Node.routeBetweenTwoNodes(root, 4, 5));
+
+    }
 }
--- src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Node.java
+++ src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Node.java
@@ -5,37 +5,79 @@ import java.util.Queue;
 
 public class Node {
     public int data;
-    public Node [] adjacent;
+    public Node[] adjacent;
     public boolean marked;
 
     public Node(int data) {
         this.data = data;
     }
 
-    public void searchDFS(Node root) {
+    public static void searchDFS(Node root) {
         if (root == null) return;
         root.marked = true;
-
-        for (Node node : root.adjacent) {
-            if (node.marked == false) {
-                searchDFS(node);
+        System.out.println(root.data);
+        if(root.adjacent != null) {
+            for (Node node : root.adjacent) {
+                if (node.marked == false) {
+                    searchDFS(node);
+                }
             }
         }
     }
 
-    public void searchBFS(Node root) {
+    public static void searchBFS(Node root) {
+        if(root == null) return;
+
         Queue<Node> queue = new LinkedList();
         root.marked = true;
         queue.add(root);
 
         while (!queue.isEmpty()) {
             Node node = ((LinkedList<Node>) queue).pop();
-            for (Node child : node.adjacent) {
-                if (child.marked == false) {
-                    child.marked = true;
-                    queue.add(child);
+            System.out.println(node.data);
+            if(node.adjacent != null) {
+                for (Node child : node.adjacent) {
+                    if (child.marked == false) {
+                        child.marked = true;
+                        queue.add(child);
+                    }
                 }
             }
         }
+    }
+
+    public static boolean routeBetweenTwoNodes(Node root,  int first, int second) {
+        if (first == second) return true;
+
+        boolean foundFirst = false;
+        boolean foundSecond = false;
+
+        Queue<Node> queue = new LinkedList();
+        root.marked = true;
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = ((LinkedList<Node>) queue).pop();
+            System.out.println(node.data);
+
+            if(node.data == first) {
+                foundFirst = true;
+            }
+
+            if(node.data == second) {
+                foundSecond = true;
+            }
+
+            if(node.adjacent != null) {
+                for (Node child : node.adjacent) {
+                    if (child.marked == false) {
+                        child.marked = true;
+                        queue.add(child);
+                    }
+                }
+            }
+        }
+
+        return foundFirst & foundSecond;
     }
 }
