fdb66968ae4824b5ce3672807ab734c5e12b0ec7
mael <mnana@takima.fr>
2018-12-05 23:12:05 +0100

    wip: list of depths

--- src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Main.java
+++ src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Main.java
@@ -42,26 +42,33 @@ public class Main {
 
         //*****************************
 
-        Node root = new Node(1);
-        Node node1 = new Node(3);
-        Node node2 = new Node(4);
-        Node node3 = new Node(6);
-        Node node4 = new Node(7);
-        Node node5 = new Node(8);
-        Node node6 = new Node(10);
-        Node node7 = new Node(13);
-        Node node8 = new Node(14);
-
-        int [] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14};
-//        int [] myArray = {1, 4, 7, 10};
-
-        Node node = Node.minimalTree(myArray);
-
-        Node.searchBFS(node);
+//        Node root = new Node(1);
+//        Node node1 = new Node(3);
+//        Node node2 = new Node(4);
+//        Node node3 = new Node(6);
+//        Node node4 = new Node(7);
+//        Node node5 = new Node(8);
+//        Node node6 = new Node(10);
+//        Node node7 = new Node(13);
+//        Node node8 = new Node(14);
+//
+//        int [] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14};
+//
+//        Node node = Node.minimalTree(myArray);
+//
+//        Node.searchBFS(node);
 //        Node.searchDFS(node);
 
-//        root.adjacent = new Node[] {
-//           node1
-//        };
+        Node root = new Node(7);
+        Node node1 = new Node(4);
+        Node node2 = new Node(6);
+        Node node3 = new Node(3);
+        Node node4 = new Node(1);
+        Node node5 = new Node(10);
+        Node node6 = new Node(13);
+
+        root.adjacent = new Node[] { node1, node2};
+        node1.adjacent = new Node[] { node3, node4};
+        node2.adjacent = new Node[] { node5, node6};
     }
 }
--- src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Node.java
+++ src/main/java/CrackingTheCodingInterview/TreesAndGraphs/Node.java
@@ -1,5 +1,8 @@
 package CrackingTheCodingInterview.TreesAndGraphs;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -36,12 +39,12 @@ public class Node {
     public static void searchBFS(Node root) {
         if (root == null) return;
 
-        Queue<Node> queue = new LinkedList();
+        Deque<Node> queue = new LinkedList();
         root.marked = true;
         queue.add(root);
 
         while (!queue.isEmpty()) {
-            Node node = ((LinkedList<Node>) queue).pop();
+            Node node = queue.pop();
             System.out.println(node.data);
             if (node.adjacent != null) {
                 for (Node child : node.adjacent) {
@@ -60,12 +63,12 @@ public class Node {
         boolean foundFirst = false;
         boolean foundSecond = false;
 
-        Queue<Node> queue = new LinkedList();
+        Deque<Node> queue = new LinkedList();
         root.marked = true;
         queue.add(root);
 
         while (!queue.isEmpty()) {
-            Node node = ((LinkedList<Node>) queue).pop();
+            Node node = queue.pop();
             System.out.println(node.data);
 
             if (node.data == first) {
@@ -90,7 +93,7 @@ public class Node {
     }
 
     public static Node minimalTree(int[] values) {
-//        if (values.length == 1) return new Node(values[0]);
+
         if (values.length == 2) {
             Node n = new Node(values[1]);
             n.adjacent = new Node[]{new Node(values[0])};
@@ -101,28 +104,98 @@ public class Node {
         int middle = values[index];
         Node root = new Node(middle);
 
-        if (values.length != 1) {
+        if (values.length == 1) return root;
 
-            int[] subNodesLeft = new int[values.length / 2];
-            int[] subNodesRight = new int[values.length - values.length / 2 -1];
+        int[] subNodesLeft = new int[values.length / 2];
+        int[] subNodesRight = new int[values.length - values.length / 2 - 1];
 
-            int j = 0;
-            for (int i = 0; i < values.length / 2; i++) {
-                subNodesLeft[j] = values[i];
-                j++;
-            }
-
-            int k = 0;
-            for (int i = values.length / 2 + 1; i < values.length; i++) {
-                subNodesRight[k] = values[i];
-                k++;
-            }
-
-            root.adjacent = new Node[]{
-                    minimalTree(subNodesLeft),
-                    minimalTree(subNodesRight)
-            };
+        int j = 0;
+        for (int i = 0; i < values.length / 2; i++) {
+            subNodesLeft[j] = values[i];
+            j++;
         }
+
+        int k = 0;
+        for (int i = values.length / 2 + 1; i < values.length; i++) {
+            subNodesRight[k] = values[i];
+            k++;
+        }
+
+        root.adjacent = new Node[]{
+                minimalTree(subNodesLeft),
+                minimalTree(subNodesRight)
+        };
+
         return root;
+    }
+
+    public static LinkedList[] listOfDepths(Node root) {
+
+        LinkedList<ArrayList<Node>> list = new LinkedList();
+        root.marked = true;
+
+
+        /*
+
+                    A
+                B       C
+            D   E       F   G
+            HI  JK      LM  NO
+
+
+        */
+        Deque<Node> q1 = new LinkedList();
+        q1.add(root);
+
+        Deque<Node> q2 = new LinkedList();
+        q2.add(root);
+
+        ArrayList<Node> nodes = new ArrayList();
+
+        while (!q1.isEmpty()) {
+
+            Node n1 = q1.pop();
+
+
+            //***************
+            Node n2 =  q2.pop();
+            nodes.add(n2); // On ajoute directement l'élément  dans la liste
+
+            if (q2.isEmpty()) {
+                // On  ajoute à la liste des noeuds
+                list.add(nodes);
+                // On réinit la liste
+                nodes = new ArrayList();
+                // On doit ajouter tout ce qui est dans la derniere queue
+//                ((LinkedList<Node>) q2).add(n1);
+
+            } else {
+                // Non vide !
+                // On doit ajouter à la liste car il reste encore des enfants !!
+            }
+            //***************
+
+            if(n1.adjacent == null) {
+                // On arrive aux feuilles
+                // On ajoute tous les éléments de "q1" dans la liste
+            }
+
+            if (n1.adjacent != null) {
+                for (Node child : n1.adjacent) {
+                    if (child.marked == false) {
+                        child.marked = true;
+                        q1.add(child);
+
+                        if(q2.isEmpty()) {
+                            // On ajoute seulement si c'est vide
+                            // On quitte cette ligne
+                            q2.add(child);
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
