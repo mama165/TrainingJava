e16f5798d0ca01b7fa764bb613ea0bb5981ff29b
mama165 <maelnana1@gmail.com>
2019-01-07 17:38:25 +0100

    feat: create a new database called company with employees, branch, etc.

--- src/main/Sql/script.sql
+++ src/main/Sql/script.sql
@@ -1,4 +1,4 @@
--- CREATE DATABASE  training;
+-- CREATE DATABASE  TRAINING;
 
 
 -- Create a table student with a student_id  as integer,
@@ -59,3 +59,78 @@ SELECT * FROM  student LIMIT 2;
 
 -- Select student with Biology
 SELECT * FROM student WHERE major='Biology';
+
+-- Select all student with id < 3
+SELECT FROM student WHERE student_id < 3;
+
+-- Select all student with id < 3 and name not equal to Jack
+SELECT * FROM student WHERE student_id < 4 AND name <> 'Jack';
+
+-- Select all student with name Claire, Kate or Mike
+SELECT * FROM student WHERE name IN('Claire', 'Kate', 'Mike');
+
+-- Select all student with name Claire, Kate or Mike and with id > 2
+SELECT * FROM student WHERE name IN('Claire', 'Kate', 'Mike') AND student_id > 2;
+
+
+
+
+-- Create table student
+-- CREATE TABLE student(student_id INT AUTO_INCREMENT, name VARCHAR(20), major VARCHAR(20), PRIMARY KEY(student_id));
+
+-- Second part
+-- Create a database called COMPANY
+CREATE DATABASE COMPANY;
+
+-- Create a table Employee
+CREATE TABLE Employee(emp_id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(40), last_name VARCHAR(40), birth_date DATE, sex VARCHAR(1), salary INT, super_id, branch_id);
+
+CREATE TABLE Employee(
+    emp_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    birth_date DATE,
+    sex VARCHAR(1),
+    salary INT ,
+    super_id INT,
+    branch_id INT
+);
+
+-- Create a table Branch
+CREATE TABLE branch(branch_id INT, branch_name VARCHAR(20), FOREIGN KEY (mgr_id ), mgr_start_date DATE);
+
+CREATE TABLE branch(
+    branch_id INT,
+    branch_name VARCHAR(40),
+    mgr_id  INT,
+    mgr_start_date DATE,
+    FOREIGN KEY mgr_id REFERENCES employee(emp_id) ON DELETE SET NULL;
+);
+
+-- Create a table client
+CREATE TABLE client(client_id INT PRIMARY KEY, client_name VARCHAR(20), FOREIGN KEY(branch_id));
+
+CREATE TABLE client(
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(20),
+    FOREIGN KEY(branch_id)
+);
+
+-- Create a table Works_With (an employee works with a client)
+
+CREATE TABLE works_With()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
