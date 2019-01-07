8d6e507e3fbf50596c36fc52783ccc67c563372a
mama165 <maelnana1@gmail.com>
2019-01-08 00:27:17 +0100

    wip: adding informations about database with employees, branch, etc.

--- src/main/Sql/script.sql
+++ src/main/Sql/script.sql
@@ -134,3 +134,43 @@ CREATE TABLE works_With()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE employee(emp_id INT PRIMARY KEY, first_name VARCHAR(40), last_name VARCHAR(40), birth_date DATE, sex VARCHAR(1), salary INT, super_id INT, branch_id INT);
+
+CREATE TABLE branch(branch_id INT, branch_name VARCHAR(40), mgr_id INT, mgr_start_date DATE, FOREIGN KEY REFERENCES employee(emp_id));
+
+
+ALTER TABLE employee ADD FOREIGN KEY employee(branch_id)  REFERENCES branch(branch_id) ON DELETE SET NULL;
+
+ALTER TABLE employee ADD FOREIGN KEY employee(super_id) REFERENCES employee(emp_id) ON DELETE SET NULL;
+
+
+CREATE TABLE client(client_id INT PRIMARY KEY, client_name VARCHAR(40), branch_id INT, FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL);
+
+CREATE TABLE works_with(emp_id INT, client_id INT, total_sales INT, PRIMARY KEY(emp_id, client_id) FOREIGN KEY (emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE, FOREIGN KEY (client_id) REFERENCES employee(client_id) ON DELETE CASCADE);
+
+CREATE TABLE branch_supplier(branch_id INT, supplier_name INT, supply_type VARCHAR(40), PRIMARY KEY(branch_id, supplier_name), FOREIGN  KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE);
+
+
+
+
+
+
+
+
+
+
+
