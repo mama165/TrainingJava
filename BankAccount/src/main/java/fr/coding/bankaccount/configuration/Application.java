eb4380b829c387cb2f94b741502ec881643c94b0
mama165 <maelnana1@gmail.com>
2019-06-12 15:38:00 +0200

    wip: extract main from starting server

--- BankAccount/src/main/java/fr/coding/bankaccount/configuration/Application.java
+++ BankAccount/src/main/java/fr/coding/bankaccount/configuration/Application.java
@@ -6,20 +6,23 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.glassfish.jersey.servlet.ServletContainer;
 
-public class Client {
-    private final static Logger logger = LoggerFactory.getLogger(Client.class);
+import java.time.Instant;
 
-    public static void main(String[] args) {
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static final int PORT = 8080;
+    private static final String PATH_SPEC = "/*";
+
+    void start() {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
 
-        Server server = new Server(8080);
+        Server server = new Server(PORT);
         server.setHandler(context);
 
         ServletHolder jerseyServlet = context.addServlet(
-                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+                org.glassfish.jersey.servlet.ServletContainer.class, PATH_SPEC);
         jerseyServlet.setInitOrder(0);
 
         // Tells the Jersey Servlet which REST service/class to load.
@@ -30,11 +33,12 @@ public class Client {
         try {
             server.start();
             server.join();
+            LOGGER.info(String.format("Server started at : %s", Instant.now()));
         } catch (Exception ex) {
-            logger.warn("An error occured when server started...");
+            LOGGER.warn("An error occured when server started...");
         } finally {
-            logger.info("Shutting down server");
             server.destroy();
+            LOGGER.info("Shutting down server");
         }
     }
 }
